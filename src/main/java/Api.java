c780322a89f8bf0985a2713b5e4735e3d457045a
razanalamri <razan.f96@outlook.com>
2023-01-24 15:21:37 +0400

    solve 2 queries

--- src/main/java/Api.java
+++ src/main/java/Api.java
@@ -7,11 +7,17 @@ import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.Scanner;
+
 import com.google.gson.Gson;
 
 public class Api {
-	
-	
+	public static String url = "jdbc:sqlserver://localhost:1433;databaseName=NYT;encrypt=true;trustServerCertificate=true";
+	public static String user = "sa";
+	public static String pass = "root";
+	public static Connection con = null;
+
+	static Scanner scanner1=new Scanner(System.in);
 	response response;
 	public response getResponse() {
 		return response;
@@ -36,12 +42,87 @@ public class Api {
 	 public static void main(String[] args) throws IOException, InterruptedException {
 		
 		 inserting.insertTable1();
-		
-	
-}
+		 
+		 
+		 System.out.println("Please choose one Option :");
+		 System.out.println("1-Which section had the most articles published on a particular day?");
+		 System.out.println("2-How many articles were published each month in the year 2021?");
+		 System.out.println("3-What are the top 10 articles with the most views?");
+		 System.out.println("4-How many articles were written by each author?");
+		 System.out.println("5-What are the top 5 sections with the most articles?");
+		 
+		 int option=scanner1.nextInt();
+		 
+		 
+		 
+		 switch(option){
+		 
+		 case 1:
+			 
+			 break;
+			 
+			 
+		 case 2:
+			 
+			 
+			 break;
+			 
+			 
+		 case 3:
+			 
+			 break;
+			 
+			 
+		 case 4:
+
+				
+					 String sql ="SELECT author, COUNT(*) AS title FROM authors1 GROUP BY author";
+			 System.out.println(sql);
+					
+			         try {
+
+			             Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+			             // Registering drivers
+			             DriverManager.registerDriver(driver);
+
+			             // Reference to connection interface
+			             con = DriverManager.getConnection(url, user,
+			                     pass);
+
+			             // Creating a statement
+			             Statement st = con.createStatement();
+			             int m = st.executeUpdate(sql);
+			             
+			             con.close();
+			         }
+			         catch (Exception ex) {
+
+			             System.err.println(ex);
+			    	 
+			     }
+
+
+			
+		 
+			 
+			  
+			 
+			 
+			 break;
+			 
+			 
+		 case 5 :
+			 
+			 
+			 
+			 
+		 }
+}}
+	 
+	 
 	
 	
 
 	
 	
-}
+
